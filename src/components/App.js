70f8825fc099add22e5008a87aa0ce2f2290d523
Louiza Mak <louiza.mak@gmail.com>
2024-02-29 15:55:04 -0600

    complete core deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([]);
+  const [plates, setPlates] = useState([]);
+  const [total, setTotal] = useState(100);
+
+  useEffect(() => {
+    fetch(API)
+    .then(res => res.json())
+    .then(sushis => setSushis(sushis));
+  }, [])
+
+  useEffect(() => {
+    const updateTotal = total - plates.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    setTotal(updateTotal);
+  }, [plates])
+
+  function handleAddPlate(sushi) {
+    if(total - sushi.price >= 0) {
+      const updatePlates = [...plates, sushi.price]
+      setPlates(updatePlates);
+    } else {
+      setSushis([]);
+    }
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis} onAddPlate={handleAddPlate}/>
+      <Table plates={plates} total={total} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushis, onAddPlate}) {
+  const [fourSushi, setFourSushi] = useState([]);
+  const [slice, setSlice] = useState([4, 8]);
+  
+  useEffect(() => {
+    setFourSushi(sushis.slice(0,4))
+  }, [sushis])
+
+  function handleMoreClick() {
+    const updateSlice = slice.map(slice => slice + 4);
+    const updateFourSushi = sushis.slice(slice[0], slice[1]);
+    setFourSushi(updateFourSushi);
+    setSlice(updateSlice);
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {fourSushi.map(sushi => {
+        return <Sushi 
+        key={sushi.id} 
+        sushi={sushi}
+        onAddPlate={onAddPlate}
+        />
+      })}
+      <MoreButton onMoreClick={handleMoreClick} />
     </div>
   );
 }
